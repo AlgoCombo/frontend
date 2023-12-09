72bad31b0efd514dc50d57c7109a8379b14c9283
jaymalave <jaym.github@gmail.com>
2023-12-09 15:27:14 +0530

    Ft: setup next navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, {useEffect} from "react";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() =>{
+    router.push("/home");
+  }, [])
+
   return (
     <>
       <main className=""></main>
