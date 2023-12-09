de529244c2a95e38c1b89d75e04a22d875956073
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-12-09 06:12:25 +0530

    ft: tailwind configuration

--- src/components/Blocks/AddTokenPair.tsx
+++ src/components/Blocks/AddTokenPair.tsx
@@ -12,14 +12,17 @@ function AddTokenPair() {
     <>
       {selectedPair ? (
         <div
-          className="h-40 bg-pink-500 hover:shadow-md hover:shadow-pink-700 cursor-pointer rounded-xl text-white transition flex flex-col justify-between"
+          className="h-40 bg-primary-500 hover:shadow-md hover:shadow-primary-700 cursor-pointer rounded-xl text-white transition flex flex-col justify-between"
           onClick={() => modal1.current!.showModal()}
         >
-          <div className="flex items-center space-x-1 p-5 bg-pink-800 rounded-t-xl">
-            <img src={getIconUrl(selectedPair.token0)} className="w-5 h-5" />
-            <span>{selectedPair.token0} -</span>
-            <img src={getIconUrl(selectedPair.token1)} className="w-5 h-5" />
-            <span>{selectedPair.token1}</span>
+          <div className="flex items-center justify-between p-5 bg-primary-800 rounded-t-xl">
+            <div className="flex items-center space-x-1">
+              <img src={getIconUrl(selectedPair.token0)} className="w-5 h-5" />
+              <span>{selectedPair.token0} -</span>
+              <img src={getIconUrl(selectedPair.token1)} className="w-5 h-5" />
+              <span>{selectedPair.token1}</span>
+            </div>
+            <h1 className="text-primary-400 tracking-widest text-xs">PAIR</h1>
           </div>
           <div className="flex justify-end p-5">
             <h2 className="text-4xl font-bold">123.43</h2>
@@ -27,12 +30,19 @@ function AddTokenPair() {
         </div>
       ) : (
         <div
+          className="h-40 bg-primary-500 hover:shadow-md hover:shadow-primary-700 cursor-pointer rounded-xl text-white transition flex flex-col justify-between"
           onClick={() => modal1.current!.showModal()}
-          className="text-pink-100/80 border border-pink-400/60 shadow-md hover:shadow-pink-400 h-40 rounded-xl flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-pink-400/10 transition"
         >
-          <PlusIcon className="w-6 h-6" />
-          <h1>Add Token Pair</h1>
+          <div className="flex justify-end p-5 mt-auto">
+            <h2 className="text-3xl font-bold">Select Pair</h2>
+          </div>
         </div>
+        // <div
+        //   className="text-primary-100/80 border border-primary-400/60 shadow-md hover:shadow-primary-400 h-40 rounded-xl flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-primary-400/10 transition"
+        // >
+        //   <PlusIcon className="w-6 h-6" />
+        //   <h1>Add Token Pair</h1>
+        // </div>
       )}
 
       <dialog ref={modal1} id="my_modal_1" className="modal">
--- src/components/DragAdder/index.tsx
+++ src/components/DragAdder/index.tsx
@@ -2,6 +2,7 @@
 
 import { PlusIcon } from "@heroicons/react/20/solid";
 import AddTokenPair from "../Blocks/AddTokenPair";
+import AddAlgorithm from "../Blocks/AddAlgorithm";
 
 function DragAdder() {
   return (
@@ -10,11 +11,8 @@ function DragAdder() {
         {/* Left */}
         <div className="space-y-5">
           <AddTokenPair />
-          <div className="text-pink-100/80 border border-pink-400/60 h-40 rounded-xl flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-pink-300/10 transition">
-            <PlusIcon className="w-6 h-6" />
-            <h1>Add Algorithm</h1>
-          </div>
-          <div className="text-pink-100/80 border border-pink-400/60 h-40 rounded-xl flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-pink-300/10 transition">
+          <AddAlgorithm />
+          <div className="text-primary-100/80 border border-primary-400/60 h-40 rounded-xl flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-primary-300/10 transition">
             <PlusIcon className="w-6 h-6" />
             <h1>Add Execution Layer</h1>
           </div>
